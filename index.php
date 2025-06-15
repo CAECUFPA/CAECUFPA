1c6572235597024b67b2d19bd0a558f737c85603
CAECUFPA <caec.admfinanceiro.ufpa@gmail.com>
2025-06-15 20:20:14 -0300

    teste name site

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 <html lang="pt-br">
 
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Document</title>
- <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>🏗Caec</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
- <header>
-  <?php foreach ($pages as $key => $value) {
-   echo '<a href="?page=' . $key . '">' . ucfirst($key) . '</a> ';
-  } ?>
- </header>
+  <header>
+    <?php foreach ($pages as $key => $value) {
+      echo '<a href="?page=' . $key . '">' . ucfirst($key) . '</a> ';
+    } ?>
+  </header>
 
- <p><?php echo $pages[$page]; ?></p>
+  <p><?php echo $pages[$page]; ?></p>
 </body>
 
 </html>
